88de1447e3ec17cb6f9b2b39ce47eb91d83eb4e1
Jeff Bennett <jab6ft@virginia.edu>
2020-01-24 15:47:29 -0500

    new model formulation

--- OCAES_rules.py
+++ OCAES_rules.py
@@ -1,20 +1,32 @@
+# from pyomo.environ import *
+import pyomo.environ as pyo
+
+
 # ----------------
 # Constraint and objective functions
 # ----------------
 def energy_balance(model, t):
-    return model.E_WIND[t] + model.E_EXP[t] == model.E_Curtail[t] + model.E_GRID[t] + model.E_CMP[t]
+    return model.E_WIND[t] + model.E_EXP[t] == model.E_CURTAIL[t] + model.E_GRID[t] + model.E_CMP[t]
+
+
+def capacity_trans(model, t):
+    return model.E_GRID[t] <= model.X_WIND * model.delta_t  # TODO add to docs
 
 
 def capacity_cmp(model, t):
-    return model.E_CMP[t] <= model.X_OCAES[t] * model.delta_T
+    return model.E_CMP[t] <= model.X_OCAES * model.delta_t
 
 
 def capacity_exp(model, t):
-    return model.E_EXP[t] <= model.X_OCAES[t] * model.delta_T
+    return model.E_EXP[t] <= model.X_OCAES * model.delta_t
 
 
-def reservoir_mass_limit(model, t):
-    return model.M_RES[t] <= model.N_WELLS * model.M_WELL
+def reservoir_rate_limit(model, t):
+    return model.M_CMP[t] + model.M_EXP[t] <= model.N_WELLS * model.M_DOT_WELL # TODO update constraints
+
+def reservoir_capacity_limit(model, t):
+    M_WELL = 158E6
+    return model.M_RES[t] <= model.N_WELLS * model.M_WELL # TODO update constraints - renamed
 
 
 def mass_balance(model, t):
@@ -25,36 +37,31 @@ def mass_balance(model, t):
 
 
 def work_cmp(model, t):
-    return model.E_CMP[t] == model.m_CMP[t] * model.delta_H / model.eta_CMP
+    #return model.E_CMP[t] == model.M_CMP[t] * model.delta_H / model.eta_CMP
+    return model.E_CMP[t] == model.M_CMP[t] * model.W_CMP # TODO
+
 
 
 def work_exp(model, t):
-    return model.E_EXP[t] == model.m_EXP[t] * model.delta_H * model.eta_EXP
+    # return model.E_EXP[t] == model.M_EXP[t] * model.delta_H * model.eta_EXP
+    return model.E_EXP[t] == model.M_EXP[t] * model.W_EXP # TODO
 
 
-def revenue(model, t):
-    return model.revenue == 1  # TODO Need to sum
+def revenue(model):
+    return model.REVENUE == sum(model.E_GRID[t] * model.P[t] for t in model.t)
 
 
-def costs(model, t):
-    model.X_WIND
-    model.C_WIND
-    model.F_WIND
-    model.V_WIND
-    model.C_WELL
-    model.C_MACHINE
-    model.F_OCAES
-    model.V_OCAES
-    model.CCR
+def costs(model):
+    capital = model.CCR * (model.X_WIND * model.C_WIND + model.N_WELLS * model.C_WELL + model.X_OCAES * model.C_MACHINE)
+    fixed = model.X_WIND * model.F_WIND + model.X_OCAES * model.F_OCAES
+    variable1 = model.V_WIND * sum(model.E_WIND[t] for t in model.t)
+    variable2 = model.V_OCAES * sum(model.E_CMP[t] + model.E_EXP[t] for t in model.t)
+    return model.COSTS == capital + fixed + variable1 + variable2
 
 
-
-    return model.costs == 1  # TODO
-
-
-def profit(model, t):
-    return model.profit[t] == model.revenue - model.costs
+def profit(model):
+    return model.PROFIT == model.REVENUE - model.COSTS
 
 
 def objective(model):  # Objective - maximize profit
-    return model.profit  # TODO Need to sum
+    return model.PROFIT
