a3a82006f642d278b2d0cf648fdca084fba9a64e
EnergyModels <jab6ft@virginia.edu>
2020-07-18 21:59:35 -0400

    updated plotting

---
+++ projects/virginia/single_run/ocaes_single_run.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from OCAES import ocaes
+
+# ----------------------
+# create and run model
+# ----------------------
+data = pd.read_csv('timeseries_inputs_2019.csv')
+inputs = ocaes.get_default_inputs()
+# inputs['C_well'] = 5000.0
+# inputs['X_well'] = 50.0
+# inputs['L_well'] = 50.0
+# inputs['X_cmp'] = 0
+# inputs['X_exp'] = 0
+model = ocaes(data, inputs)
+df, s = model.get_full_results()
+
+df.to_csv('results_timeseries.csv')
+s.to_csv('results_values.csv')
+print(model.calculate_LCOE(s))
+
+# ----------------------
+# create plots using built-in functions
+# ----------------------
+model.plot_overview()
+model.plot_power_energy()
