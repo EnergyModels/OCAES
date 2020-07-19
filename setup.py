fcb05fc29420128e7bc4d64c5ded1de275f68e12
EnergyModels <jab6ft@virginia.edu>
2020-07-19 11:39:38 -0400

    turned off energy arbitrage for COVE optimization

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='OCAES',
-      version='0.0.4',
+      version='0.0.5',
       description='Technoeconomic optimization of offshore compressed air energy storage (OCAES) systems',
       url='https://github.com/EnergyModels/OCAES',
       author='Jeff Bennett',
