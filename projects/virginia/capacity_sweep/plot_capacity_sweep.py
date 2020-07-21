4ec46093360eb0e1dc688221c7633cc64f9722e5
EnergyModels <jab6ft@virginia.edu>
2020-07-21 08:33:58 -0400

    Updated plotting

--- projects/virginia/capacity_sweep/plot_capacity_sweep.py
+++ projects/virginia/capacity_sweep/plot_capacity_sweep.py
@@ -14,18 +14,28 @@ savename = "Fig_Optimization_Results.png"
 DPI = 400  # Set resolution for saving figures
 
 x_vars = ["capacity"]
-x_labels = ["Power Rating\n(MW)"]
+x_labels = ["Storage power rating (MW)"]
 x_converts = [1.0, 1.0]
 x_limits = [[], []]
 
-y_vars = ["LCOE", "COVE", "revenue", "avoided_emissions"]
-y_labels = ["LCOE\n($/kWh)", "COVE\n($/kWh)", "Revenue\n($/kWh)", "Avoided Emissions\n(ton/MWh)"]
-y_converts = [1.0, 1.0, 1.0, 1.0]
-y_limits = [[], [], []]
+y_vars_all = ["COVE", "LCOE", "revenue", "avoided_emissions"]
+y_labels_all = ["COVE\n($/kWh)", "LCOE\n($/kWh)", "Revenue\n($/kWh)", "Avoided emissions\n(t/MWh)"]
+y_converts_all = [1.0, 1.0, 1.0, 1.0]
+y_limits_all = [[], [], [], []]
 
 series_var = 'scenario'
+series = ['wind_only', '4_hr_batt', '10_hr_batt', '10_hr_ocaes', '24_hr_ocaes']
 series_dict = {'wind_only': 'Wind only', '4_hr_batt': 'Battery (4 hr)', '10_hr_batt': 'Battery (10 hr)',
                '10_hr_ocaes': 'OCAES (10 hr)', '24_hr_ocaes': 'OCAES (24 hr)'}
+
+# Set Color Palette
+# colors = sns.color_palette("Paired")
+# colors = [(0, 0, 0), colors[0], colors[1], colors[2], colors[3]]
+
+colors = sns.color_palette()
+colors = [(0, 0, 0), colors[0], colors[5], colors[2], colors[1]]  # black, blue, brown, green, orange
+linestyles = ['solid', 'solid', 'solid', 'dashed', 'dotted']
+markers = ['o', 's', 'D', '^', '.', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'o', 'X']
 
 # =====================================
 # process data
@@ -34,94 +44,112 @@ series_dict = {'wind_only': 'Wind only', '4_hr_batt': 'Battery (4 hr)', '10_hr_b
 # Import results
 df = pd.read_csv(results_filename)
 
-# get list of unique series
-series = df.scenario.unique()
+for timeseries_filename in df.timeseries_filename.unique():
+    n = len(y_vars_all)
+    if timeseries_filename == "timeseries_inputs_2015.csv":
+        savename = "Fig_Optimization_Results_2015.png"
+        n = 3
+    elif timeseries_filename == "timeseries_inputs_2017.csv":
+        savename = "Fig_Optimization_Results_2017.png"
+    elif timeseries_filename == "timeseries_inputs_2019.csv":
+        savename = "Fig_Optimization_Results_2019.png"
+    elif timeseries_filename == "timeseries_inputs_multiyear.csv":
+        savename = "Fig_Optimization_Results_multiyear.png"
+        n = 3
+    print(savename)
 
-# =====================================
-# create plots
-# =====================================
+    y_vars = y_vars_all[0:n]
+    y_labels = y_labels_all[0:n]
+    y_converts = y_converts_all[0:n]
+    y_limits = y_limits_all[0:n]
 
-# Column width guidelines https://www.elsevier.com/authors/author-schemas/artwork-and-media-instructions/artwork-sizing
-# Single column: 90mm = 3.54 in
-# 1.5 column: 140 mm = 5.51 in
-# 2 column: 190 mm = 7.48 i
-width = 7.48  # inches
-height = 5.5  # inches
+    # =====================================
+    # create plots
+    # =====================================
 
-# Create plot
-f, a = plt.subplots(len(y_vars), len(x_vars), sharex='col', sharey='row', squeeze=False)
+    # Column width guidelines https://www.elsevier.com/authors/author-schemas/artwork-and-media-instructions/artwork-sizing
+    # Single column: 90mm = 3.54 in
+    # 1.5 column: 140 mm = 5.51 in
+    # 2 column: 190 mm = 7.48 i
+    width = 7.48  # inches
+    height = 6.5  # inches
 
-# Set size
-f.set_size_inches(width, height)
+    # Create plot
+    f, a = plt.subplots(len(y_vars), len(x_vars), sharex='col', sharey='row', squeeze=False)
 
-# Set style and context
-sns.set_style("white", {"font.family": "serif", "font.serif": ["Times", "Palatino", "serif"]})
-sns.set_context("paper")
-sns.set_style("ticks", {"xtick.major.size": 8, "ytick.major.size": 8})
+    # Set size
+    f.set_size_inches(width, height)
 
-# Set Color Palette
-colors = sns.color_palette("colorblind")
+    # Set style and context
+    sns.set_style("white", {"font.family": "serif", "font.serif": ["Times", "Palatino", "serif"]})
+    sns.set_context("paper")
+    sns.set_style("ticks", {"xtick.major.size": 8, "ytick.major.size": 8})
 
-# Set marker shapes and sizes
-markers = ['o', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'o', 'X']
-marker_size = 4
-markeredgewidth = 1.0
+    # Set marker shapes and sizes
+    marker_size = 4
+    markeredgewidth = 1.0
+    linewidth = 2.5
 
-count = 0
-# iterate through x-variables
-for i, (x_var, x_label, x_convert, x_limit) in enumerate(zip(x_vars, x_labels, x_converts, x_limits)):
+    count = 0
+    # iterate through x-variables
+    for i, (x_var, x_label, x_convert, x_limit) in enumerate(zip(x_vars, x_labels, x_converts, x_limits)):
 
-    # iterate through y-variables
-    for j, (y_var, y_label, y_convert, y_limit) in enumerate(zip(y_vars, y_labels, y_converts, y_limits)):
+        # iterate through y-variables
+        for j, (y_var, y_label, y_convert, y_limit) in enumerate(zip(y_vars, y_labels, y_converts, y_limits)):
 
-        # access subplot
-        ax = a[j, i]
+            # access subplot
+            ax = a[j, i]
 
-        # iterate through series
-        for k, (serie, color, marker) in enumerate(zip(series, colors, markers)):
-            ind = df.loc[:, series_var] == serie
-            x = x_convert * df.loc[ind, x_var]
-            y = y_convert * df.loc[ind, y_var]
-            ax.plot(x, y, linestyle='', marker=marker, markersize=marker_size,
-                    markeredgewidth=markeredgewidth, markeredgecolor=color, markerfacecolor='None',
-                    label=series_dict[serie])
+            # iterate through series
+            for k, (serie, color, marker, linestyle) in enumerate(zip(series, colors, markers, linestyles)):
+                ind = (df.loc[:, "timeseries_filename"] == timeseries_filename) & (df.loc[:, series_var] == serie)
+                x = x_convert * df.loc[ind, x_var]
+                y = y_convert * df.loc[ind, y_var]
 
-        # axes labels
-        # x-axis labels (only bottom)
-        if j == len(y_vars) - 1:
-            ax.set_xlabel(x_label)
-        else:
-            ax.get_xaxis().set_visible(False)
+                # points
+                ax.plot(x, y, linestyle='', marker=marker, markersize=marker_size,
+                        markeredgewidth=markeredgewidth, markeredgecolor=color, markerfacecolor='None',
+                        label=series_dict[serie])
+                #
+                # lines
+                # ax.plot(x, y, color=color, marker='None', linewidth=linewidth, linestyle=linestyle, label=series_dict[serie])
 
-        # y-axis labels (only left side)
-        if i == 0:
-            ax.set_ylabel(y_label)
-        else:
-            ax.get_yaxis().set_visible(False)
+            # axes labels
+            # x-axis labels (only bottom)
+            if j == len(y_vars) - 1:
+                ax.set_xlabel(x_label)
+            else:
+                ax.get_xaxis().set_visible(False)
 
-        # Despine and remove ticks
-        sns.despine(ax=ax, )
-        ax.tick_params(top=False, right=False)
+            # y-axis labels (only left side)
+            if i == 0:
+                ax.set_ylabel(y_label)
+            else:
+                ax.get_yaxis().set_visible(False)
 
-        # Axes limits
+            # Despine and remove ticks
+            sns.despine(ax=ax, )
+            ax.tick_params(top=False, right=False)
 
-        # Caption labels
-        caption_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']
-        plt.text(0.1, 0.9, caption_labels[count], horizontalalignment='center', verticalalignment='center',
-                 transform=ax.transAxes, fontsize='medium', fontweight='bold')
-        count = count + 1
+            # Axes limits
 
-# Legend
-# y_pos = j / 2 + 0.5
-# leg = a[j, i].legend(bbox_to_anchor=(1.2, y_pos), ncol=1, loc='center')
-x_pos = -0.1
-leg = a[j, i].legend(bbox_to_anchor=(x_pos, -0.6), ncol=3, loc='upper center')
+            # Caption labels
+            caption_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']
+            plt.text(-0.075, 1.05, caption_labels[count], horizontalalignment='center', verticalalignment='center',
+                     transform=ax.transAxes, fontsize='medium', fontweight='bold')
+            count = count + 1
 
-# Adjust layout
-# plt.tight_layout()
-plt.subplots_adjust(hspace=0.2, wspace=0.2, bottom=0.2)
-f.align_ylabels(a[:, 0])  # align y labels
+    # Legend
+    # y_pos = j / 2 + 0.5
+    # leg = a[j, i].legend(bbox_to_anchor=(1.2, y_pos), ncol=1, loc='center')
+    x_pos = 0.45
+    leg = a[j, i].legend(bbox_to_anchor=(x_pos, -0.6), ncol=5, loc='upper center')
 
-# Save Figure
-plt.savefig(savename, dpi=DPI, bbox_extra_artists=leg)
-# plt.close()
+    # Adjust layout
+    # plt.tight_layout()
+    plt.subplots_adjust(hspace=0.2, wspace=0.2, bottom=0.2)
+    f.align_ylabels(a[:, 0])  # align y labels
+
+    # Save Figure
+    plt.savefig(savename, dpi=DPI, bbox_extra_artists=leg)
+    # plt.close()
